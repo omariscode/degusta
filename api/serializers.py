e055db04b1673f606776b67ee3c70a5b60f1cda3
Omar Rodrigues <omarscode007@gmail.com>
2026-01-18 15:33:20 +0100

    Feat: Update function on user serializers for update infos endpoint

--- api/serializers.py
+++ api/serializers.py
@@ -14,7 +14,7 @@ from .models import (
 
 
 class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, required=True)
+    password = serializers.CharField(write_only=True, required=False)
 
     class Meta:
         model = user_model.User
@@ -43,6 +43,18 @@ class UserSerializer(serializers.ModelSerializer):
         user.is_active = True
         user.save()
         return user
+    
+    def update(self, instance, validated_data):
+        password = validated_data.pop("password", None)
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        if password:
+            instance.set_password(password)
+
+        instance.save()
+        return instance
 
 
 class AdminRegisterSerializer(serializers.ModelSerializer):
